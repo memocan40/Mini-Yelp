50e40a5ec212e82ea80e5cba5ea9e3e139952f51
memo40 <memocann4040@gmail.com>
2021-01-29 14:53:25 +0100

    add restaurantdetailed component

--- src/components/restaurants/RestaurantDetailed/index.js
+++ src/components/restaurants/RestaurantDetailed/index.js
@@ -1,4 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
-export default function RestaurantDetailed(){}
+import GoBackButton from '../../GoBackButton';
+
+import './index.css';
+
+export default function RestaurantDetailed({ restaurants }) {
+  return (
+    <article className="restaurants-entry">
+      <div className="restaurants-entry__left-wrapper">
+        <h3 className="restaurants-entry__title">{restaurants.name}</h3>
+        <img className="restaurants-entry__image" src={restaurants.image} alt="" />
+      </div>
+      <div className="restaurants-entry__right-wrapper">
+        <GoBackButton
+          className="restaurants-entry__home-btn"
+          caption="back"
+          home="no"
+        />
+        <div className="restaurants-entry__content">
+          <div className="restaurants-entry__date">{restaurants.tag}</div>
+          <div className="restaurants-entry__line"></div>
+          <div className="restaurants-entry__description">{restaurants.city}</div>
+          <div className="restaurants-entry__line"></div>
+          <div className="restaurants-entry__rating">
+            
+            
+          </div>
+        
+          
+        </div>
+      </div>
+    </article>
+  );
+}

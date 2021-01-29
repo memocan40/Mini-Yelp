ee3267a3e60c23e8d1f28640689d0bee8ac55059
DavidSurina <david.surina22@gmail.com>
2021-01-29 16:54:00 +0100

    css changes

--- src/components/restaurants/Restaurant/index.js
+++ src/components/restaurants/Restaurant/index.js
@@ -8,6 +8,7 @@ export default function Restaurant({ object }) {
               <img class="restaurant-img" src={`${object.Img_url}`} alt={`${object.Name}`}/>
             </div>
             <h3 class="restaurant-title">{`${object.Name}`}</h3>
+            <div class="restaurant-description">{`${object.Description}`}</div>
             <div class="restaurant-tag-wrapper">
               {object.tags.map((tag) => {
                 return <div class="restaurant-tag">{tag.Name}</div>

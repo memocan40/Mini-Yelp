6eafb4317050eec082204ce81b641de926122308
DavidSurina <david.surina22@gmail.com>
2021-01-29 13:00:56 +0100

    saving changes before pull

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
@@ -41,19 +44,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
